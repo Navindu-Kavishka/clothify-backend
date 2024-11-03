962bb3c7f85314f26b5a834a4679072a675d7d09
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-11-03 11:14:04 +0530

    started backend

---
+++ src/main/java/com/nk/clothify_backend/ClothifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nk.clothify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nk/clothify_backend/ClothifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk.clothify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
