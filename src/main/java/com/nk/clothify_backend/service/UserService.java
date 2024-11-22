7ad0f9a803a8046f9792615e977896c7d2c91711
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-11-22 20:22:29 +0530

    fix warns

--- src/main/java/com/nk/clothify_backend/service/UserService.java
+++ src/main/java/com/nk/clothify_backend/service/UserService.java
@@ -1,13 +1,12 @@
 package com.nk.clothify_backend.service;
 
 
-import com.nk.clothify_backend.entity.UserEntity;
 import com.nk.clothify_backend.exception.UserException;
 import com.nk.clothify_backend.model.User;
 
 public interface UserService {
 
-    public User findUserById(Long userId) throws UserException;
+    User findUserById(Long userId) throws UserException;
 
-    public User findUserProfileByJwt(String jwt) throws UserException;
+    User findUserProfileByJwt(String jwt) throws UserException;
 }
